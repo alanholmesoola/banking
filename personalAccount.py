7989b2184275566b29fce1bd4b4683610f5e8a8c
Alan Holmes <alanholmesoola@yahoo.ie>
2019-07-17 20:30:32 +0100

    change

--- personalAccount.py
+++ personalAccount.py
@@ -3,8 +3,9 @@ from account import Account
 
 class PersonalAccount(Account):
 
-    def __init__(self):
+    def __init__(self, name, age):
         self.overdraft = 200
+        super(PersonalAccount, self).__init__(name, age)
         
         
     

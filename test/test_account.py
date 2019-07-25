b264e087f106d98163e23f1f79c989180bdd28db
David Power <david.power@wpengine.com>
2019-07-25 09:17:38 +0100

    Update the gitignore to ignore .ds files. Move the README back to the root (its a good convention to follow) and apply linting updates to test_account

--- test/test_account.py
+++ test/test_account.py
@@ -1,19 +1,18 @@
+import unittest
 from app.account import Account
 
-import unittest
 
 class TestAccount(unittest.TestCase):
 
-#Positive Cases
-
+    # Positive Cases
     def deposit(self):
         # Arrange
-        account = Account("Alan","33")
+        account = Account("Alan", "33")
         expected_result = 100
-        
+
         # Act
         account.deposit(100)
         observed = account.balance
-        
+
         # Assert
         self.assertEqual(observed, expected_result)

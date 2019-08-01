727bb2987c8f9d1a4a6236d31279770c3e27d115
David Power <david.power@wpengine.com>
2019-08-01 15:17:20 +0100

    Added initial database class

--- app/accountBuilder.py
+++ app/accountBuilder.py
@@ -4,13 +4,13 @@ from app.personalAccount import PersonalAccount
 
 
 class AccountBuilder:
-    def build(self, acc_type, name, age):
+    def build(self, acc_type, name, age, balance=0):
         # create instance of account class
         if acc_type == 'personal':
-            return PersonalAccount(name, age)
+            return PersonalAccount(name, age, balance)
 
         if acc_type == "business":
-            return BusinessAccount(name, age)
+            return BusinessAccount(name, age, balance)
 
         if acc_type == "student":
-            return StudentAccount(name, age)
+            return StudentAccount(name, age, balance)

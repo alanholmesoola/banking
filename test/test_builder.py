38708ffa8c3f8a25bf8c6564f860e86d71065f80
David Power <david.power@wpengine.com>
2019-08-04 19:09:27 +0100

    Update test_builder.py

--- test/test_builder.py
+++ test/test_builder.py
@@ -1,43 +1,23 @@
 import unittest
+from parameterized import parameterized
 
 from app.studentAccount import StudentAccount
 from app.businessAccount import BusinessAccount
 from app.personalAccount import PersonalAccount
 from app.accountBuilder import AccountBuilder
-from parameterized import parameterized
 
 
 class TestBuilder(unittest.TestCase):
 
-    # Positive Cases
-    def test_person_account_is_created_using_builder(self):
-        # Arrange
-        account_builder = AccountBuilder()
-        observed_class = account_builder.build('personal', 'Alan', 22)
-        # Assert
-        self.assertIsInstance(observed_class, PersonalAccount)
-
-    def test_business_account_is_created_using_builder(self):
-        # Arrange
-        account_builder = AccountBuilder()
-        observed_class = account_builder.build('business', 'Alan', 22)
-        # Assert
-        self.assertIsInstance(observed_class, BusinessAccount)
-
-    def test_student_account_is_created_using_builder(self):
-        # Arrange
-        account_builder = AccountBuilder()
-        observed_class = account_builder.build('student', 'Alan', 22)
-        # Assert
-        self.assertIsInstance(observed_class, StudentAccount)
-
-    @parameterized([
-        ("business", BusinessAccount),
-        ("personal", PersonalAccount),
-        ("student", StudentAccount),
+    @parameterized.expand([
+        ("business", 'Alan', 33, BusinessAccount),
+        ("personal", 'David', 32, PersonalAccount),
+        ("student", 'Marie', 32, StudentAccount),
     ])
-    def test_accounts_using_builder_parameterised(self, userInput, expected):
+    def test_account_builder_can_build(self, acc_type, test_name, test_age, expected):
         # arrange
-        observed_class = account_builder.build(userInput, 'Alan', 22)
+        account_builder = AccountBuilder()
+        observed_class = account_builder.build(acc_type, test_name, test_age)
+
         # assert
         self.assertIsInstance(observed_class, expected)
